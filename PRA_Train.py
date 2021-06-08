1225fa411d2df140ddf16d540a7145fb6a80f5ac
Tyler_Leigh <tyler@tylerleigh.live>
2021-06-08 19:52:35 +0800

    完善测试函数

--- PRA_Train.py
+++ PRA_Train.py
@@ -10,6 +10,7 @@ from collections import OrderedDict
 from sklearn.model_selection  import train_test_split
 from sklearn.linear_model import LogisticRegression
 import joblib
+import pickle
 import time
 import math
 
@@ -236,105 +237,110 @@ class PRA_Model():
         for i in range(0, len(listTemp),n):
             yield listTemp[i:i + n]
 
-    def compute_feature(self,path,fromnode,tonode,direction=1):
+    def compute_feature(self,path,fromnode,tonode):
         """
         计算路径特征值,后向截断的策略
         path是关系路径一个包含关系的元组，例:({'relationship':'HAS'},{'relationship':'IN_COUNTRY'},{'relationship':'IN_LEAGUE'})
         fromnode是路径的头节点:例{'label':'Person','name':'liqiang'}或id?
         tonode是路径的尾节点  :例{'label':'Person','name':'hanmeimei'}或id?
         """
-        #Prl = 0 #probability of reaching node e from e' with a one step random walk with relationship type Rl
-        #nodee_1 = 0 #e'
-        #nodee   = 0 #e
-        #range_P_1 = 0 #range of P'
         length = len(path) #length of path
         hspe = 0 #the path feature value
-        if direction == 0:#注意正向迭代迭代
-            if len(path)==1:
-                query = "MATCH (N),(M)-[r:"+path[0]['relationshipType']+"]->(tar)"
-                #print()
-                if self.Graph.run(query).data()[0]=="true":
-                    hspe = 1/n
-                else:
-                    hspe = 0
+        if length == 1:#关系路径长度只剩1
+            query = "MATCH (N),(M)-[r:"+path[0]['relationshipType']+"]->(tar) WHERE id(N)="+str(tonode)+"AND id(M)="+str(fromnode)+"WITH N,collect(tar) AS target RETURN N IN target"
+            data = self.Graph.run(query).data()
+            if not data:
+                hspe = 0
+            elif data[0]['N IN target']==True:#如果到节点与从节点通过关系链接的话
+                query_end = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN count(N)"
+                hspe = 1/self.Graph.run(query_end).data()[0]['count(N)']
+            else:
+                hspe = 0
+            return hspe
+        else:#关系路径长度大于1
+            query_start = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN count(N)"
+            nodes_count = self.Graph.run(query_start).data()[0]['count(N)']#首先找出从节点通过关系0链接的节点数
+            if nodes_count == 0:
+                return 0
+            else:
+                query_middle = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN id(N)"
+                nodes = self.Graph.run(query_middle).data()#找出链接的节点
+                for entity in nodes:
+                    hspe += (1/nodes_count) * self.compute_feature(path[1:],entity['id(N)'],tonode)
                 return hspe
-            #else:
-
-            #for relation in path:
-            #    #首先找到本次关系的尾节点,py2neo.database.work.Cursor
-            #    Endnodes = self.Graph.run('MATCH ()-[r:'+relation['relationship']+']->() RETURN endNode(r)')
-            #    for endnode in Endnodes:#endnode为record类型,endnode[0]即为node类型
-            #        print(endnode[0])
-            #        print(type(endnode[0]))
-            #        break
-            #    break
-        elif direction == 1:#注意反向迭代
-            if length == 1:#关系路径长度只剩1
-                query = "MATCH (N),(M)-[r:"+path[0]['relationshipType']+"]->(tar) WHERE id(N)="+str(tonode)+"AND id(M)="+str(fromnode)+"WITH N,collect(tar) AS target RETURN N IN target"
-                data = self.Graph.run(query).data()
-                if not data:
-                    hspe = 0
-                elif data[0]['N IN target']==True:#如果到节点与从节点通过关系链接的话
-                    query_end = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN count(N)"
-                    hspe = 1/self.Graph.run(query_end).data()[0]['count(N)']
-                else:
-                    hspe = 0
-                return hspe
-            else:#关系路径长度大于1
-                query_start = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN count(N)"
-                nodes_count = self.Graph.run(query_start).data()[0]['count(N)']#首先找出从节点通过关系0链接的节点数
-                if nodes_count == 0:
-                    return 0
-                else:
-                    query_middle = "MATCH (M)-[r:"+path[0]['relationshipType']+"]->(N) WHERE id(M)="+str(fromnode)+"RETURN id(N)"
-                    nodes = self.Graph.run(query_middle).data()#找出链接的节点
-                    for entity in nodes:
-                        hspe += (1/nodes_count) * self.compute_feature(path[1:],entity['id(N)'],tonode)
-                    return hspe
         return hspe
 
-    def data_construt(self,paths):
+    def data_construt(self,paths,scale,config,mode):
         """构建训练集与测试集数据"""
-        querysids = "MATCH (m:"+self.predicted_relation[1]+") WITH id(m) AS sid LIMIT 10 RETURN collect(sid) as sids"#找出路径首位节点id
-        sids = self.Graph.run(querysids).data()[0]['sids']
-        querytids = "MATCH (m:"+self.predicted_relation[2]+") WITH id(m) AS tid LIMIT 10 RETURN collect(tid) as tids"
-        tids = self.Graph.run(querytids).data()[0]['tids']
-        node_pairs = [0]*len(sids)*len(tids)
-        i = 0
-        for sid in sids:#得到所有的节点对组合
-            for tid in tids:
-                node_pairs[i] = [sid,tid]
-                i += 1
-        i = 0
-        feature_matrice = [0]*len(paths)
-        node_pair_feature = []
-        labels = []
-        data = {}
-        t1 = time.clock()
-        for path in paths:#计算对应节点对的路径特征向量
-            for node_pair in node_pairs:
-                path_feature = self.compute_feature(path,node_pair[0],node_pair[1])#计算路径特征值
-                if i == 0:
-                    if path_feature == 0:#判断关系标签,只运行一遍
-                        labels = labels + [0]
-                    else:
-                        labels = labels + [1]
-                node_pair_feature = node_pair_feature + [path_feature]
-            feature_matrice[i] = node_pair_feature
-            data['path'+str(i)] = node_pair_feature
+        #querysids = "MATCH (m:"+self.predicted_relation[1]+") WITH id(m) AS sid LIMIT 10 RETURN collect(sid) as sids"#找出路径首位节点id
+        #sids = self.Graph.run(querysids).data()[0]['sids']
+        #querytids = "MATCH (m:"+self.predicted_relation[2]+") WITH id(m) AS tid LIMIT 10 RETURN collect(tid) as tids"
+        #tids = self.Graph.run(querytids).data()[0]['tids']
+        if mode == 0:
+            labels_qualified = self.load_labels('Labels.csv','labels.txt',1)
+            querysids = "MATCH (m:"+self.predicted_relation[1]+") WHERE id(m)<>1400 WITH id(m) AS sid LIMIT "+str(scale)+" RETURN collect(sid) as sids"#找出路径首位节点id
+            sids = self.Graph.run(querysids).data()[0]['sids']
+            querytids = "MATCH (m:"+self.predicted_relation[2]+") WITH id(m) AS tid LIMIT "+str(scale)+" RETURN collect(tid) as tids"
+            tids = self.Graph.run(querytids).data()[0]['tids']
+            node_pairs = [0]*len(sids)*len(tids)
+            i = 0
+            for sid in sids:#得到所有的节点对组合
+                for tid in tids:
+                    node_pairs[i] = [sid,tid]
+                    i += 1
+            i = 0
             node_pair_feature = []
-            #print('process time spent:',time.clock()-t1)
-            i += 1        
-        data['relation_labels'] = labels
+            labels = []
+            data = {}
+            for path in paths:#计算对应节点对的路径特征向量
+                time_start = time.time() #开始计时
+                for node_pair in node_pairs:
+                    path_feature = self.compute_feature(path,node_pair[0],node_pair[1])#计算路径特征值
+                    if i == 0:
+                        if node_pair in labels_qualified:
+                            labels = labels + [1]
+                        else:
+                            labels = labels + [0]
+                    node_pair_feature = node_pair_feature + [path_feature]
+                data['path'+str(i)] = node_pair_feature
+                time_end = time.time() #开始计时
+                print('Path',i,'Computed,','Time Cost:',time_end-time_start,'s')
+                node_pair_feature = [] 
+                i += 1  
+
+            data['relation_labels'] = labels
+            with open(config['data_name'],'wb') as f:
+                pickle.dump(data,f)
+
+        #feature_matrice = [0]*len(paths)
+        #node_pair_feature = []
+        #labels = []
+        #data = {}
+        #t1 = time.clock()
+        #for path in paths:#计算对应节点对的路径特征向量
+        #    for node_pair in node_pairs:
+        #        path_feature = self.compute_feature(path,node_pair[0],node_pair[1])#计算路径特征值
+        #        if i == 0:
+        #            if path_feature == 0:#判断关系标签,只运行一遍
+        #                labels = labels + [0]
+        #            else:
+        #                labels = labels + [1]
+        #        node_pair_feature = node_pair_feature + [path_feature]
+        #    feature_matrice[i] = node_pair_feature
+        #    data['path'+str(i)] = node_pair_feature
+        #    node_pair_feature = []
+        #    #print('process time spent:',time.clock()-t1)
+        #    i += 1        
+        elif mode == 1:
+            with open(config['data_name'],'rb') as f:
+                data = pickle.load(f)
         train_data = pd.DataFrame(data)#转换为DataFrame
         print('训练数据格式:')
         print(train_data.head())
         i = 0
         exam_X = train_data.loc[:,['path'+str(i) for i in range(len(paths))]]#提取特征向量与标签
         exam_Y = train_data.loc[:,'relation_labels']
-        X_train,X_test,Y_train,Y_test=train_test_split(exam_X,exam_Y,train_size= .8)#分开训练集与测试集数据
-        #X_train=X_train.values.reshape(-1,1)
-        #X_test=X_test.values.reshape(-1,1)
+        X_train,X_test,Y_train,Y_test=train_test_split(exam_X,exam_Y,train_size= 0.7)#分开训练集与测试集数据
         return X_train,X_test,Y_train,Y_test
 
     def sfe_data_construct(self,paths,scale,main_col,config):
--- SFE_Train.py
+++ SFE_Train.py
@@ -63,51 +63,53 @@ class SFE_Model(PRA_Model):
                 labels = pickle.load(f)
                 return labels
 
-    def sfe_data_construct(self,paths,scale,main_col,config):
+    def sfe_data_construct(self,paths,scale,main_col,config,mode):
         """
         训练集与测试集构建
         """
-        labels_qualified = SFE.load_labels('Labels.csv','labels.txt',1)
-        querysids = "MATCH (m:"+self.predicted_relation[1]+") WHERE id(m)<>1400 WITH id(m) AS sid LIMIT "+str(scale)+" RETURN collect(sid) as sids"#找出路径首位节点id
-        sids = self.Graph.run(querysids).data()[0]['sids']
-        querytids = "MATCH (m:"+self.predicted_relation[2]+") WITH id(m) AS tid LIMIT "+str(scale)+" RETURN collect(tid) as tids"
-        tids = self.Graph.run(querytids).data()[0]['tids']
-        node_pairs = [0]*len(sids)*len(tids)
-        i = 0
-        for sid in sids:#得到所有的节点对组合
-            for tid in tids:
-                node_pairs[i] = [sid,tid]
-                i += 1
-        i = 0
-        node_pair_feature = []
-        labels = []
-        data = {}
-        for path in paths:#计算对应节点对的路径特征向量
-            time_start = time.time() #开始计时
-            for node_pair in node_pairs:
-                path_feature = self.sfe_compute_feature(path,node_pair[0],node_pair[1])#计算路径特征值
-                #if i == main_col:
-                #    if path_feature == 0:#判断关系标签,只运行一遍
-                #        labels = labels + [0]
-                #    else:
-                #        labels = labels + [1]
-                if node_pair in labels_qualified:
-                    labels = labels + [1]
-                else:
-                    labels = labels + [0]
-                node_pair_feature = node_pair_feature + [path_feature]
-            data['path'+str(i)] = node_pair_feature
-            time_end = time.time() #开始计时
-            print('Path',i,'Computed,','Time Cost:',time_end-time_start,'s')
-            node_pair_feature = [] 
-            i += 1  
+        if mode == 0:
+            labels_qualified = self.load_labels('Labels.csv','labels.txt',1)
+            querysids = "MATCH (m:"+self.predicted_relation[1]+") WHERE id(m)<>1400 WITH id(m) AS sid LIMIT "+str(scale)+" RETURN collect(sid) as sids"#找出路径首位节点id
+            sids = self.Graph.run(querysids).data()[0]['sids']
+            querytids = "MATCH (m:"+self.predicted_relation[2]+") WITH id(m) AS tid LIMIT "+str(scale)+" RETURN collect(tid) as tids"
+            tids = self.Graph.run(querytids).data()[0]['tids']
+            node_pairs = [0]*len(sids)*len(tids)
+            i = 0
+            for sid in sids:#得到所有的节点对组合
+                for tid in tids:
+                    node_pairs[i] = [sid,tid]
+                    i += 1
+            i = 0
+            node_pair_feature = []
+            labels = []
+            data = {}
+            for path in paths:#计算对应节点对的路径特征向量
+                time_start = time.time() #开始计时
+                for node_pair in node_pairs:
+                    path_feature = self.sfe_compute_feature(path,node_pair[0],node_pair[1])#计算路径特征值
+                    #if i == main_col:
+                    #    if path_feature == 0:#判断关系标签,只运行一遍
+                    #        labels = labels + [0]
+                    #    else:
+                    #        labels = labels + [1]
+                    if i == 0:
+                        if node_pair in labels_qualified:
+                            labels = labels + [1]
+                        else:
+                            labels = labels + [0]
+                    node_pair_feature = node_pair_feature + [path_feature]
+                data['path'+str(i)] = node_pair_feature
+                time_end = time.time() #开始计时
+                print('Path',i,'Computed,','Time Cost:',time_end-time_start,'s')
+                node_pair_feature = [] 
+                i += 1  
 
-        data['relation_labels'] = labels
-        with open(config['data_name'],'wb') as f:
-            pickle.dump(data,f)
-
-        #with open(config['data_name'],'rb') as f:
-        #    data = pickle.load(f)
+            data['relation_labels'] = labels
+            with open(config['data_name'],'wb') as f:
+                pickle.dump(data,f)
+        elif mode == 1:
+            with open(config['data_name'],'rb') as f:
+                data = pickle.load(f)
 
         train_data = pd.DataFrame(data)#转换为DataFrame
         print('训练数据格式:')
@@ -115,8 +117,30 @@ class SFE_Model(PRA_Model):
         i = 0
         exam_X = train_data.loc[:,['path'+str(i) for i in range(len(paths))]]#提取特征向量与标签
         exam_Y = train_data.loc[:,'relation_labels']
-        X_train,X_test,Y_train,Y_test=train_test_split(exam_X,exam_Y,train_size= 0.8)#分开训练集与测试集数据
+        X_train,X_test,Y_train,Y_test=train_test_split(exam_X,exam_Y,train_size= 0.7)#分开训练集与测试集数据
         return X_train,X_test,Y_train,Y_test
+    
+    def sfe_acc_path_select(self,paths,label_file,path_num):
+        """
+        通过训练集标签中的数据进一步筛选路径
+        paths:待选择的路径
+        label_file:正例集合
+        path_num:返回路径个数
+        """
+        potential_paths = []
+        paths_acc = [0]*len(paths)
+        path_acc_temp = []
+        labels_qualified = SFE.load_labels('Labels.csv',label_file,1)#首先读出标签集
+        n = len(labels_qualified)
+        for i,path in enumerate(paths):
+            for label in labels_qualified:
+                path_acc_temp.append(self.sfe_compute_feature(path,label[0],label[1]))
+            paths_acc[i] = sum(path_acc_temp)/n
+            path_acc_temp = []
+        index = sorted(range(len(paths_acc)), key=lambda k: paths_acc[k],reverse = True)#返回降序排列acc的索引
+        for index_num in index[:path_num]:
+            potential_paths.append(paths[index_num])
+        return potential_paths
 
     def sfe_train(self,X_train,Y_train,X_test,Y_test,config):
         """SFE训练"""
@@ -144,73 +168,150 @@ class SFE_Model(PRA_Model):
         print('pre_data:',pre_data,' shape:',pre_data.shape)
         print(self.Model.predict_proba(pre_data))
         print(self.Model.predict(pre_data))
-        weights = self.Model.coef_
-        print('Weights:',weights,'Bias:',self.Model.intercept_)
+        #weights = self.Model.coef_
+        #print('Weights:',weights,'Bias:',self.Model.intercept_)
 
 if __name__=="__main__":
     Military = Graph('http://localhost:7474',auth=('neo4j','ne1013pk250'),name='military66')
  
-    #def SFE_rw_model_test(Graph,predicted_relationship):
-    #    SFE_rw = SFE_Model(Graph,predicted_relationship)
-    #    #start_time = time.time()
-    #    #paths = PRA.find_paths(mode='randomwalk',config={'length':3,'example_num':10,'sample_num':1000,'steps':20})
-    #    #end_time =  time.time()
-    #    #print("搜索用时:",end_time-start_time,'s')
-    #    #with open('path_pra_country.txt','wb') as f:
-    #    #    pickle.dump(paths,f)
-    #    with open('path_pra_country.txt','rb') as f:
-    #        paths = pickle.load(f)
-    #    start_time = time.time()
-    #    X_train,X_test,Y_train,Y_test = SFE_rw.sfe_data_construct(paths,60,2,{'data_name':'sfe_rw_data.txt'})
-    #    end_time =  time.time()
-    #    print("用时:",end_time-start_time,'s')
-    #    start_time = time.time()
-    #    #PRA.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':'sfe_rw_30.model'})
-    #    SFE_rw.Model = joblib.load('sfe_rw_30.model')
-    #    print('准确率:'+str(SFE_rw.Model.score(X_train+X_test,Y_train+Y_test)))
-    #    end_time =  time.time()
-    #    print("训练用时:",end_time-start_time,'s')
+    def PRA_model_test(Graph,predicted_relationship,mode,file_path,predict_nodes_pair=None):
+        """
+        mode:0-训练，1-预测
+        """
+        PRA = SFE_Model(Graph,predicted_relationship)
+        if mode == 0:
+            #start_time = time.time()
+            #paths = PRA.find_paths(mode='randomwalk',config={'length':3,'example_num':10,'sample_num':1000,'steps':20})
+            #end_time =  time.time()
+            #print("搜索用时:",end_time-start_time,'s')
+            #with open(file_path+'/path/path_pra_randomwalk.txt','wb') as f:
+            #    pickle.dump(paths,f)
+            with open(file_path+'/path/path_pra_randomwalk.txt','rb') as f:
+                paths = pickle.load(f)
+            start_time = time.time()
+            X_train,X_test,Y_train,Y_test = PRA.data_construt(paths,45,{'data_name':file_path+'/dataset/pra_rw_data.txt'},mode=0)
+            end_time =  time.time()
+            print("构建训练集用时:",end_time-start_time,'s')
+            start_time = time.time()
+            PRA.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':file_path+'/model/pra_rw_45.model'})
+            end_time =  time.time()
+            print("训练用时:",end_time-start_time,'s')
+        elif mode == 1:
+            with open(file_path+'/path/path_pra_randomwalk.txt','rb') as f:
+                paths = pickle.load(f)
+            start_time = time.time()
+            SFE_rw.sfe_predict(predict_nodes_pair[0],predict_nodes_pair[1],paths,file_path+'/model/pra_rw_45.model')
+            end_time =  time.time()
+            print("预测用时:",end_time-start_time,'s')
+            return
 
-    #def SFE_BFS_model_test(Graph,predicted_relationship):
-    #    SFE_BFS = SFE_Model(Graph,predicted_relationship)
-    #    #start_time = time.time()
-    #    #paths = SFE_BFS.find_paths(mode='BFS',config={'length':3,'example_num':10,'constraint':20})
-    #    #end_time =  time.time()
-    #    #print("搜索用时:",end_time-start_time,'s')
-    #    #with open('path_sfe_bfs_country.txt','wb') as f:
-    #    #    pickle.dump(paths,f)
-    #    with open('path_sfe_bfs_country.txt','rb') as f:
+    def SFE_rw_model_test(Graph,predicted_relationship,mode,file_path,predict_nodes_pair=None):
+        """
+        mode:0-训练，1-预测
+        """
+        SFE_rw = SFE_Model(Graph,predicted_relationship)
+        if mode == 0:
+            start_time = time.time()
+            paths = SFE_rw.find_paths(mode='randomwalk',config={'length':3,'example_num':10,'sample_num':1000,'steps':20})
+            end_time =  time.time()
+            print("搜索用时:",end_time-start_time,'s')
+            with open(file_path+'/path/path_sfe_randomwalk.txt','wb') as f:
+                pickle.dump(paths,f)
+            start_time = time.time()
+            X_train,X_test,Y_train,Y_test = SFE_rw.sfe_data_construct(paths,45,2,{'data_name':file_path+'/dataset/sfe_rw_data.txt'},mode=0)
+            end_time =  time.time()
+            print("构建训练集用时:",end_time-start_time,'s')
+            start_time = time.time()
+            SFE_rw.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':file_path+'/model/sfe_rw_45.model'})
+            end_time =  time.time()
+            print("训练用时:",end_time-start_time,'s')
+        elif mode == 1:
+            with open(file_path+'/path/path_sfe_randomwalk.txt','rb') as f:
+                paths = pickle.load(f)
+            start_time = time.time()
+            SFE_rw.sfe_predict(predict_nodes_pair[0],predict_nodes_pair[1],paths,file_path+'/model/sfe_rw_45.model')
+            end_time =  time.time()
+            print("预测用时:",end_time-start_time,'s')
+            return
+
+    def SFE_BFS_model_test(Graph,predicted_relationship,mode,file_path,predict_nodes_pair=None):
+        """
+        mode:0-训练，1-预测
+        """
+        SFE_BFS = SFE_Model(Graph,predicted_relationship)
+        if mode == 0:
+            #start_time = time.time()
+            #paths = SFE_BFS.find_paths(mode='BFS',config={'length':3,'example_num':10,'constraint':20})
+            #end_time =  time.time()
+            #print("搜索用时:",end_time-start_time,'s')
+            #with open('path_sfe_bfs_country.txt','wb') as f:
+            #    pickle.dump(paths,f)
+            with open(file_path+'/path/path_sfe_bfs_country.txt','rb') as f:
+                paths = pickle.load(f)
+            start_time = time.time()
+            X_train,X_test,Y_train,Y_test = SFE_BFS.sfe_data_construct(paths,45,2,{'data_name':file_path+'/dataset/sfe_bfs_data.txt'},mode=0)
+            end_time =  time.time()
+            print("构建训练集用时:",end_time-start_time,'s')
+            start_time = time.time()
+            SFE_BFS.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':file_path+'/model/sfe_bfs_45.model'})
+            print('准确率:'+str(SFE_BFS.Model.score(X_test,Y_test)))
+            end_time =  time.time()
+            print("训练用时:",end_time-start_time,'s')    
+        elif mode == 1:
+            with open(file_path+'/path/path_sfe_bfs_country.txt','rb') as f:
+                paths = pickle.load(f)
+            start_time = time.time()
+            SFE_BFS.sfe_predict(predict_nodes_pair[0],predict_nodes_pair[1],paths,file_path+'/model/sfe_bfs_45.model')
+            end_time =  time.time()
+            print("预测用时:",end_time-start_time,'s')
+            return  
+
+    #def Military_inference(Graph,predicted_relationship,mode):
+    #    """
+    #    mode:0-训练，1-预测
+    #    """
+    #    SFE = SFE_Model(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'])
+    #    with open('sfe_path_acc_select30_deletenull.txt','rb') as f:
     #        paths = pickle.load(f)
-    #    start_time = time.time()
-    #    X_train,X_test,Y_train,Y_test = SFE_BFS.sfe_data_construct(paths,30,157,{'data_name':'sfe_bfs_data.txt'})
-    #    end_time =  time.time()
-    #    print("用时:",end_time-start_time,'s')
-    #    start_time = time.time()
-    #    SFE_BFS.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':'sfe_bfs_30.model'})
-    #    #SFE_BFS.Model = joblib.load('sfe_rw_30.model')
-    #    print('准确率:'+str(SFE_BFS.Model.score(X_train+X_test,Y_train+Y_test)))
-    #    end_time =  time.time()
-    #    print("训练用时:",end_time-start_time,'s')    
-    
-    ##SFE_rw_model_test(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'])
-    #SFE_BFS_model_test(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'])
+    #    time_start = time.time() #开始计时
+    #    useful_paths = SFE.sfe_acc_path_select(paths,'labels.txt',30)
+    #    time_end = time.time() 
+    #    print('acc筛选用时:',time_end-time_start,'s')
+    #    time_start = time.time() #开始计时
+    #    X_train,X_test,Y_train,Y_test = SFE.sfe_data_construct(paths,45,38,{'data_name':'dataset_path30_newlabel_45_1_deletenull.txt'})
+    #    time_end = time.time() 
+    #    print('构建训练集总用时:',time_end-time_start,'s')
+    #    weights = SFE.Model.coef_
+    #    y = weights.argsort()
+    #    for sample in y[0][:5]:
+    #        print('Top weight',weights[0][sample],'path',paths[sample])
+    #    for sample in y[0][-5:]:
+    #        print('Bottom weight',weights[0][sample],'path',paths[sample])
+    #SFE_rw_model_test(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'],mode=1,file_path='Temp_Files/SFE_rw',predict_nodes_pair=[3533,17])
+    #PRA_model_test(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'],mode=0,file_path='Temp_Files/PRA')
+    SFE_BFS_model_test(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'],mode=0,file_path='Temp_Files/SFE_bfs')
    
 
-    SFE = SFE_Model(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'])
+    #SFE = SFE_Model(Graph=Military,predicted_relationship=['生产单位位于','生产单位','产国'])
     ##paths = SFE.find_paths(mode='BFS',config={'length':3,'example_num':10,'constraint':20,'sample_num':1000,'strategy':'stratified'})
     
     #with open('path.txt','wb') as f:
     #    pickle.dump(paths,f)
 
-    with open('path.txt','rb') as f:
-        paths = pickle.load(f)
+    #with open('sfe_path_acc_select30_deletenull.txt','rb') as f:
+    #    paths = pickle.load(f)
 
-    SFE.Model = joblib.load('sfe_60_1.model')
-    time_start = time.time() #开始计时
-    X_train,X_test,Y_train,Y_test = SFE.sfe_data_construct(paths,60,38,{'data_name':'dataset_newlabel_60.txt'})
-    time_end = time.time() #开始计时
-    print('构建训练集总用时:',time_end-time_start,'s')
-    print('准确率:'+str(SFE.Model.score(X_test,Y_test)))
+    #time_start = time.time() #开始计时
+    #useful_paths = SFE.sfe_acc_path_select(paths,'labels.txt',30)
+    #time_end = time.time() 
+    #print('acc筛选用时:',time_end-time_start,'s')
+    ##with open('sfe_path_acc_select30_deletenull.txt','wb') as f:
+    ##    pickle.dump(useful_paths,f)
+    #print(useful_paths)
+    #time_start = time.time() #开始计时
+    #X_train,X_test,Y_train,Y_test = SFE.sfe_data_construct(paths,45,38,{'data_name':'dataset_path30_newlabel_45_1_deletenull.txt'})
+    #time_end = time.time() 
+    #print('构建训练集总用时:',time_end-time_start,'s')
     #weights = SFE.Model.coef_
     #y=weights.argsort()
     #for sample in y[0][:5]:
@@ -219,8 +320,15 @@ if __name__=="__main__":
     #    print('Bottom weight',weights[0][sample],'path',paths[sample])
     #print('Weights:',weights,'Bias:',self.Model.intercept_)
 
-    #SFE.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':'sfe_60_1.model'})
-    #SFE.sfe_predict(15863,34,paths,'sfe_60.model')
+    #SFE.sfe_train(X_train,Y_train,X_test,Y_test,{'model_name':'dataset_path30_newlabel_45_1_deletenull.model'})
+    #SFE.Model = joblib.load('sfe_newlabel_45_1.model')
+    #print('Model has been loaded')
+    #weights = SFE.Model.coef_
+    #y=weights.argsort()
+    #for sample in y[0][:5]:
+    #    print('Top weight',weights[0][sample],'path',paths[sample])
+    #for sample in y[0][-5:]:
+    #    print('Bottom weight',weights[0][sample],'path',paths[sample])
 
     #SFE.sfe_compute_feature(({'relationship':'r生产单位'},{'relationship':'产国'}),3533,17)
 
